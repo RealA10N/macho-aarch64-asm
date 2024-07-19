bdc2668515fa57812e130a5faaf8b54ae51bb17a
Alon Krymgand <me@alon.kr>
2024-07-19 18:19:29 +0300

    Tidy add register tests

--- aarch64/instructions/add_register_test.go
+++ aarch64/instructions/add_register_test.go
@@ -1,49 +1,50 @@
 package instructions_test
 
 import (
-	"macho-aarch64-asm/aarch64/instructions"
-	"macho-aarch64-asm/aarch64/registers"
 	"testing"
+
+	"github.com/RealA10N/macho-aarch64-asm/aarch64/instructions"
+	"github.com/RealA10N/macho-aarch64-asm/aarch64/registers"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSimpleWordAddition(t *testing.T) {
 	w0, err := registers.NewGeneratePurposeRegister(0, false)
-	assert.NoError(t, err, "expected valid register")
+	assert.NoError(t, err)
 
 	w1, err := registers.NewGeneratePurposeRegister(1, false)
-	assert.NoError(t, err, "expected valid register")
+	assert.NoError(t, err)
 
 	inst, err := instructions.NewAddRegister(w0, w0, w1)
-	assert.NoError(t, err, "expected instruction")
+	assert.NoError(t, err)
 
 	assert.Equal(t, inst.String(), "ADD w0, w0, w1")
 }
 
 func TestSimpleExtendedAddition(t *testing.T) {
 	x1, err := registers.NewGeneratePurposeRegister(1, true)
-	assert.NoError(t, err, "expected valid register")
+	assert.NoError(t, err)
 
 	x2, err := registers.NewGeneratePurposeRegister(2, true)
-	assert.NoError(t, err, "expected valid register")
+	assert.NoError(t, err)
 
 	x3, err := registers.NewGeneratePurposeRegister(3, true)
-	assert.NoError(t, err, "expected valid register")
+	assert.NoError(t, err)
 
 	inst, err := instructions.NewAddRegister(x1, x2, x3)
-	assert.NoError(t, err, "expected instruction")
+	assert.NoError(t, err)
 
 	assert.Equal(t, inst.String(), "ADD x1, x2, x3")
 }
 
 func TestWrongRegisterTypes(t *testing.T) {
 	w0, err := registers.NewGeneratePurposeRegister(0, false)
-	assert.NoError(t, err, "expected valid register")
+	assert.NoError(t, err)
 
 	x1, err := registers.NewGeneratePurposeRegister(1, true)
-	assert.NoError(t, err, "expected valid register")
+	assert.NoError(t, err)
 
 	_, err = instructions.NewAddRegister(w0, w0, x1)
-	assert.Error(t, err, "expected invalid instruction")
+	assert.Error(t, err)
 }
