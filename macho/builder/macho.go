1a3013f90702218e26c3ef2d08b050a9cdbf40df
Alon Krymgand <me@alon.kr>
2024-07-22 22:40:28 +0300

    Added draft implementation of MachoBuilder

--- macho/builder/macho.go
+++ macho/builder/macho.go
@@ -1,19 +1,81 @@
-package macho
+package builder
 
-import "github.com/RealA10N/macho-aarch64-asm/macho/header"
+import (
+	"encoding"
+	"fmt"
+	"io"
 
-type machoBuilder struct {
-	header header.MachoHeader
+	"github.com/RealA10N/macho-aarch64-asm/macho/header"
+)
+
+type CommandHeaderBuilder interface {
+	io.WriterTo
+}
+type CommandDataBuilder interface {
+	io.WriterTo
 }
 
-func MachoBuilder(cpu header.CpuType, subtype header.CpuSubType, filetype header.FileType, flags header.Flags) machoBuilder {
-	return machoBuilder{
-		header: header.MachoHeader{
-			Magic:      cpu.ToMagic(),
-			CpuType:    cpu,
-			CpuSubType: subtype,
-			FileType:   filetype,
-			Flags:      flags,
-		},
+type CommandBuilder interface {
+	GetHeaderSize() int64
+	SetOffset(offest int64) (CommandHeaderBuilder, CommandDataBuilder)
+}
+
+type MachoBuilder struct {
+	Header   header.MachoHeader
+	Commands []CommandBuilder
+}
+
+func WriteTo(obj encoding.BinaryMarshaler, writer io.Writer) (int, error) {
+	data, err := obj.MarshalBinary()
+	if err != nil {
+		return 0, err
 	}
+	return writer.Write(data)
+}
+
+func (macho MachoBuilder) allHeadersSize() (n int64) {
+	for _, cmd := range macho.Commands {
+		n += cmd.GetHeaderSize()
+	}
+	return
+}
+
+func (macho MachoBuilder) WriteTo(writer io.Writer) (n int64, err error) {
+	var k int64
+
+	n, err = macho.Header.WriteTo(writer)
+	if err != nil {
+		return
+	}
+
+	dataBuilders := []CommandDataBuilder{}
+	dataOffset := n + macho.allHeadersSize()
+
+	for _, cmd := range macho.Commands {
+		header, data := cmd.SetOffset(dataOffset)
+
+		k, err = header.WriteTo(writer)
+		n += k
+		if err != nil {
+			return
+		}
+
+		dataBuilders = append(dataBuilders, data)
+		dataOffset += k
+	}
+
+	if n != dataOffset {
+		err = fmt.Errorf("expected macho header %d (actual %d)", dataOffset, n)
+		return
+	}
+
+	for _, data := range dataBuilders {
+		k, err = data.WriteTo(writer)
+		n += k
+		if err != nil {
+			return
+		}
+	}
+
+	return
 }
